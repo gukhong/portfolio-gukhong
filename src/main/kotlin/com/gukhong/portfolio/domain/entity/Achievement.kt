818e5bd4ac30f25305c9521ff3f174d6e0dd074a
gukhong <gukhong@gmail.com>
2024-05-04 00:16:10 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/gukhong/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/gukhong/portfolio/domain/entity/Achievement.kt
@@ -1,13 +1,31 @@
 package com.gukhong.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(title: String,
+                  description: String,
+                  achievedDate: LocalDate?,
+                  host: String,
+                  isActive: Boolean
+) : BaseEntity() {
 
     @Id // 이 필드가 PK임을 표시
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK생성 전략 IDENTIRY = 생성을 DB에 위임
     @Column(name = "achievement_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var achivedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
+
+
 
 }

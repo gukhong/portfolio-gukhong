3201f9af55716706cfef3c8b8fa6841465cad81e
gukhong <gukhong@gmail.com>
2024-06-25 16:15:33 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/gukhong/portfolio/admin/advice/AdminApiControllerAdvice.java
+++
@@ -1,7 +0,0 @@
-package com.gukhong.portfolio.admin.advice;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class AdminApiControllerAdvice {
-}

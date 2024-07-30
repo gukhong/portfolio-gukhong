c78f1e78d935e9b07f0ee87799e2cdd475f61232
hong <gukhong@gmail.com>
2024-07-30 11:55:33 +0900

    Dashboard개발 - controller, service, DTO

---
+++ src/main/kotlin/com/gukhong/portfolio/admin/context/dashboard/data/VisitorsDTO.kt
@@ -0,0 +1,9 @@
+package com.gukhong.portfolio.admin.context.dashboard.data
+
+class VisitorsDTO(
+    val name: String,
+    val count: Long,
+    val color: String,
+    val background: String
+) {
+}

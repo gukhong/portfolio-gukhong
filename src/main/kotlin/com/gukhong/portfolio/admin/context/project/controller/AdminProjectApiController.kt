936bc5f00badbdff551474900ee13e17150f50ee
hong <gukhong@gmail.com>
2024-09-06 18:30:30 +0900

    저장안되는 문제 해결(api/ POST요청 시 복수형 적용안했었음).

--- src/main/kotlin/com/gukhong/portfolio/admin/context/project/controller/AdminProjectApiController.kt
+++ src/main/kotlin/com/gukhong/portfolio/admin/context/project/controller/AdminProjectApiController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/admin/api/project")
+@RequestMapping("/admin/api/projects")
 class AdminProjectApiController(
     private val adminProjectService: AdminProjectService
 ) {
